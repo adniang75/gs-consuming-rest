ef1468456735811d9faedbe7f2c5877cd693817a
Alassane NIANG <adniang75@gmail.com>
2021-10-05 15:56:59 +0200

    Initial commit

---
+++ src/test/java/com/alassaneniang/gsconsumingrest/GsConsumingRestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.alassaneniang.gsconsumingrest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsConsumingRestApplicationTests {
+
+/*    @Test
+    void contextLoads () {
+    }*/
+
+}
